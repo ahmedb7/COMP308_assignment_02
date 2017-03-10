831ae58e349c518d2f06b78e7063f03da2eef755
Benzir Ahmed <benzir.ahmed7@gmail.com>
2017-03-09 21:08:11 -0500

    added content pages

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb://localhost/videogames"
-  //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames";
+  //Remote MongoDB deployment -> let URI = "mongodb://ahmedb7:1234@ds127300.mlab.com:27300/business_contacts";
 };
